141780c1981122ec49ad2b187f0cacf8dc8d6b3f
Redcom1988 <earthsea27@gmail.com>
2024-12-03 11:31:37 +0800

    changed news content to be button and in a scaffold

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 class NewsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('News Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ecoscan'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print('Button Pressed');
+            },
+            child: Text('Button'),
+          ),
+        ],
+      ),
     );
   }
 }
