ce2c868a8644e623fe9e2709fd7121281547b14e
Redcom1988 <earthsea27@gmail.com>
2024-12-13 18:53:44 +0800

    placeholder login auth

---
+++ lib/backend-client/login_authentication.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:crypto/crypto.dart';
+
+Future<bool> loginUser(String email, String password) async {
+  // Hash the password using SHA-256
+  var bytes = utf8.encode(password);
+  var digest = sha256.convert(bytes);
+
+  final response = await http.post(
+    Uri.parse('http://localhost/login'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'email': email,
+      'passwordHash': digest.toString(),
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
