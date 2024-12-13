ef8f5bcfa54e88b51914df5b5bad95477119c34a
Redcom1988 <earthsea27@gmail.com>
2024-12-13 23:53:59 +0800

    removed decoupled code from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_state.dart';
-import 'screens/home_screen.dart';
-import 'screens/news_screen.dart';
-import 'screens/voucher_screen.dart';
-import 'screens/profile_screen.dart';
-import 'screens/qrscanner_screen.dart';
+import 'screens/login_screen.dart'; // Import the login screen
 
 void main() {
   runApp(MyApp());
@@ -24,60 +20,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final List<Widget> _screens = [
-    HomeScreen(),
-    NewsScreen(),
-    VouchersScreen(),
-    ProfileScreen(),
-    QRScannerScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: _screens[appState.selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: appState.selectedIndex,
-        onTap: (index) {
-          appState.setIndex(index);
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.newspaper),
-            label: 'News',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code_scanner),
-            label: 'Scan',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.airplane_ticket),
-            label: 'Vouchers',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
-          ),
-        ],
-        type: BottomNavigationBarType.fixed,
-        elevation: 8,
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        unselectedItemColor:
-            Theme.of(context).colorScheme.onSurface.withOpacity(0.6),
+        home: LoginScreen(), // Set login screen as the home
       ),
     );
   }
