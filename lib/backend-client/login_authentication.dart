4230bace04bd98400045b74bb5f3df230134ff6a
Redcom1988 <earthsea27@gmail.com>
2024-12-14 20:01:40 +0800

    added shared_preferences var save, changed uri to portforwarded link

--- lib/backend-client/login_authentication.dart
+++ lib/backend-client/login_authentication.dart
@@ -1,16 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart';
-import '../models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginResponse {
   final bool success;
-  final User? user;
   final String? error;
 
   LoginResponse({
     required this.success,
-    this.user,
     this.error,
   });
 }
@@ -23,7 +21,7 @@ Future<LoginResponse> loginUser(String email, String password) async {
   try {
     final response = await http
         .post(
-          Uri.parse('http://localhost:3000/users/login'),
+          Uri.parse('https://w4163hhc-3000.asse.devtunnels.ms/users/login'),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8',
           },
@@ -35,26 +33,37 @@ Future<LoginResponse> loginUser(String email, String password) async {
         .timeout(Duration(seconds: 10));
 
     if (response.statusCode == 200) {
+      print("Login Success");
       final Map<String, dynamic> data = json.decode(response.body);
 
       // Create User object from response data
-      final user = User(
-        email: email,
-        username: data['username'] ?? '',
-        fullName: data['fullName'] ?? '',
-      );
+      // final user = User(
+      //   email: email,
+      //   username: data['username'] ?? '',
+      //   fullName: data['fullName'] ?? '',
+      // );
+
+      final username = data['username'] ?? '';
+      final fullName = data['fullName'] ?? '';
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('email', email);
+      await prefs.setString('username', username);
+      await prefs.setString('fullName', fullName);
 
       return LoginResponse(
         success: true,
-        user: user,
       );
     } else {
+      print("QWE");
       return LoginResponse(
         success: false,
         error: 'Login failed. Please try again.',
       );
     }
   } catch (e) {
+    String error = "${e.toString()}ASD";
+    print(error);
     return LoginResponse(
       success: false,
       error: 'Network error. Please check your connection.',
