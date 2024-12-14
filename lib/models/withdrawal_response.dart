83ce72dc09cfdb88b21f2be9837b72df20327cbe
Redcom1988 <earthsea27@gmail.com>
2024-12-14 20:39:29 +0800

    new withdrawalId in response model

--- lib/models/withdrawal_response.dart
+++ lib/models/withdrawal_response.dart
@@ -1,9 +1,11 @@
 class WithdrawalResponse {
   final bool success;
   final String? error;
+  final int? withdrawalId;
 
   WithdrawalResponse({
     required this.success,
     this.error,
+    this.withdrawalId,
   });
 }
